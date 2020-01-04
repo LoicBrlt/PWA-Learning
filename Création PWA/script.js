2ed46cd80bf58b7a7429142b9c053b323dc09b25
Barault <54353090+LoicBrlt@users.noreply.github.com>
2020-01-04 14:01:54 +0100

    Fichiers de base pour PWA
    Ne contient pas de scripts pour la mise en cache hors ligne.

---
+++ Création PWA/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if ('serviceWorker' in navigator) {
+    	navigator.serviceWorker
+        	.register('sw.js')
+        	.then(serviceWorker => {
+        		console.log('Service Worker registered: ' + serviceWorker);
+			})
+		.catch(error => {
+        	console.log('Error registering the Service Worker: ' + error);
+		});
+	}
+});
